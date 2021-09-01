5ed3fb984d4debd8d9b346fef754b47dbb6e8e3f
Billy He <billy@metalabdesign.com>
2021-09-01 13:33:50 -0700

    Implement FindClosest node type

--- src/node-type/colour/index.js
+++ src/node-type/colour/index.js
@@ -1,5 +1,7 @@
+import findClosest from "./find-closest";
 import hexToRGB from "./hex-to-rgb";
 
 export const registerNodes = (prefix, register, context) => {
+  register(`${prefix}find-closest`, findClosest(context));
   register(`${prefix}hex-to-rgb`, hexToRGB(context));
 };
