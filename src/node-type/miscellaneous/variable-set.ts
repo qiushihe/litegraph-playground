0dd02f4611e161be3e1cebf163489c7cdd7f18cb
Billy He <billy@metalabdesign.com>
2021-09-09 17:46:36 -0700

    Fix an issue with VariableSet sending signal too early

--- src/node-type/miscellaneous/variable-set.ts
+++ src/node-type/miscellaneous/variable-set.ts
@@ -33,6 +33,15 @@ class VariableSetNode extends BaseNode {
           this.getInputData(1)
         );
       }
+
+      // We can not trigger the output slot at the same time as the value of the variable is
+      // updated in the storage object. The reason for that is because if we don't wait another
+      // cycle, any corresponding `VariableGet` node would be return stale value because those
+      // value are obtained at the same time as the triggering of the signal. By waiting another
+      // cycle, we can ensure any corresponding `VariableGet` node gets the updated value before
+      // this signal is triggered.
+      this.enqueueTask("send-signal");
+    } else if (taskName === "send-signal") {
       this.triggerSlot(0, taskParam);
     }
   }
