412194810a25edce35e825054911ad3258a20846
Billy He <billy@metalabdesign.com>
2021-09-13 17:04:00 -0700

    Add Text output node type

--- src/node-type/output/index.ts
+++ src/node-type/output/index.ts
@@ -4,6 +4,7 @@ import ConsoleNode from "./console";
 import ConsoleLogNode from "./console-log";
 import ImageNode from "./image";
 import TableNode from "./table";
+import TextNode from "./text";
 
 export const registerNodes = (
   prefix: string,
@@ -14,4 +15,5 @@ export const registerNodes = (
   register(`${prefix}console-log`, ConsoleLogNode);
   register(`${prefix}image`, ImageNode);
   register(`${prefix}table`, TableNode);
+  register(`${prefix}text`, TextNode);
 };
